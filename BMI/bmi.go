b59bc96eb93cb266850df54bbc9e80a2a1719f3c
Elshater <elshater.ehab@gmail.com>
2023-08-01 08:33:31 +0300

    fetching user input

--- BMI/bmi.go
+++ BMI/bmi.go
@@ -1,16 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
 
 func main() {
 	//output information
 	fmt.Println("BMI Calculator")
-fmt.Println("---------------------------------")
+	fmt.Println("---------------------------------")
 	//prompt for user input
 	fmt.Println("Enter your weght in (kg): ")
+
+	weightInput, _ := reader.ReadString('\n')
+
 	fmt.Print("Enter your height in (m): ")
 
+	heightInput, _ := reader.ReadString('\n')
+
+	fmt.Print(weightInput, heightInput)
 	//saving user inputs to variables
 	//calculate BMI (weight/(height*height))
-	//output the calculator BMI  
+	//output the calculator BMI
 }
